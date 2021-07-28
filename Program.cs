7976fffeb502c927d65d776174e303539e2994e1
Alexin <Alexinael@gmail.com>
2021-07-28 18:38:02 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HomeWork5._5
+{
+    
+    class Program
+    {
+
+        static int KeyInputInt(string text)
+        {
+            string s = KeyInputString(text);
+
+            int ret = 0;
+
+            Int32.TryParse(s, out ret);
+
+            return ret;
+        }
+        static string KeyInputString(string text)
+        {
+            Console.WriteLine($"Input string ({text})");
+
+            return Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
